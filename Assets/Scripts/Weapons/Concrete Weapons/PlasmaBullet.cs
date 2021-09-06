66a8cac5b26a4a2705cd8d4418c049a8bc0e6b79
Kyle Grenier <kyle.grenier11@gmail.com>
2021-09-06 01:15:30 -0500

    Plasma bullet bounce SFX

--- Assets/Scripts/Weapons/Concrete Weapons/PlasmaBullet.cs
+++ Assets/Scripts/Weapons/Concrete Weapons/PlasmaBullet.cs
@@ -11,15 +11,34 @@ using UnityEngine;
 // TODO: Destroyed in a number of bounced instead of a time??
 public class PlasmaBullet : Bullet
 {
+    [Tooltip("The number of times the bullet will bounce until it is destroyed.")]
+    [SerializeField] private int maxBounces;
+    /// <summary>
+    /// The number of times the bullet has bounced.
+    /// </summary>
+    private int currentBounces;
+
+    [Header("Audio")]
+    [Tooltip("The SFX Channel.")]
+    [SerializeField] private AudioClipChannelSO sfxChannel;
+
+    [Tooltip("The AudioClipSO that holds the bounce SFX.")]
+    [SerializeField] private AudioClipSO bounceSFX;
+
     /// <summary>
     /// Destroy the bullet on collision with game object tagged "Player".
     /// </summary>
     /// <param name="collision">The Collision2D the bullet collided with.</param>
     protected override void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") || currentBounces >= maxBounces)
         {
             Destroy(gameObject);
+        }
+        else
+        {
+            sfxChannel.RaiseEvent(bounceSFX);
+            ++currentBounces;
         }
     }
 }

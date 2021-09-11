5760067c795c48097cb5d89080d01f346e13031f
Kyle Grenier <kyle.grenier11@gmail.com>
2021-09-11 06:15:58 -0500

    Creating interfaces for guards

---
+++ Assets/Scripts/ISiphonable.cs
@@ -0,0 +1,12 @@
+/*****************************************************************************
+// File Name :         ISiphonable.cs
+// Author :            Kyle Grenier
+// Creation Date :     09/11/2021
+//
+// Brief Description : An interface all game objects that the can have their health siphoned / leeched must implement.
+*****************************************************************************/
+
+public interface ISiphonable
+{
+    void OnSiphoned();
+}
